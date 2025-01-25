492d028001b3615a0e7ce892e0e1be388db4ba06
Paul Gestwicki <pvgestwicki@bsu.edu>
2025-01-25 15:00:24 -0500

    Make material instance private

--- project/launcher/wand.gd
+++ project/launcher/wand.gd
@@ -3,11 +3,11 @@ extends Node3D
 var color : Color:
 	set(value):
 		color = value
-		material.albedo_color = color
+		_material.albedo_color = color
 
-var material : StandardMaterial3D
+var _material : StandardMaterial3D
 
 func _ready() -> void:
-	material = $Handle.material.duplicate()
-	$Handle.material = material
-	$Loop.material = material
+	_material = $Handle.material.duplicate()
+	$Handle.material = _material
+	$Loop.material = _material
