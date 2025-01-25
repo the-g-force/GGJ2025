4d077234ac11b2f952a168f7c0bf183bc1dbc0e3
Paul Gestwicki <pvgestwicki@bsu.edu>
2025-01-25 10:27:23 -0500

    Add popping bubbles when they leave the board
    There's an area that defines where the board is. When the bubbles
    leave that area, they are queue_free'd.

--- project/world.gd
+++ project/world.gd
@@ -6,3 +6,9 @@ func _physics_process(_delta: float) -> void:
 		%LeftLauncher.action_pressed()
 	if Input.is_action_just_pressed("fire_right"):
 		%RightLauncher.action_pressed()
+
+
+func _on_board_zone_body_exited(body: Node3D) -> void:
+	if body is RigidBody3D:
+		body.queue_free()
+		print("pop!")
