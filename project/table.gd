9bb2091ece62b38d5522ce4ab5d3fd988cbf5759
Paul Gestwicki <pvgestwicki@bsu.edu>
2025-01-24 20:21:16 -0500

    Attempt tilting the board

--- project/table.gd
+++ project/table.gd
@@ -1,6 +1,7 @@
-extends StaticBody3D
-
+extends CSGBox3D
 
 
 func _physics_process(_delta: float) -> void:
-	%LeftHinge["motor/enable"] = Input.is_action_pressed("flipper_left")
+	rotation.z = Input.get_joy_axis(0, JOY_AXIS_LEFT_X) * 0.5
+	rotation.x = Input.get_joy_axis(0, JOY_AXIS_LEFT_Y) * 0.5
+#	%LeftHinge["motor/enable"] = Input.is_action_pressed("flipper_left")
