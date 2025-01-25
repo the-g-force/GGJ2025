b2d26e09dc9e61b1faf12d364268a6045a1e120f
Paul Gestwicki <pvgestwicki@bsu.edu>
2025-01-25 15:09:36 -0500

    Add pegs
    Peg-pegs.

--- project/table/table.gd
+++ project/table/table.gd
@@ -1,5 +1,8 @@
 extends StaticBody3D
 
+@export var pegs := 6
+@export var peg_radius := 3.0
+
 @onready var _spike_multimesh : MultiMesh = $SpikeMultiMeshInstance.multimesh
 
 
@@ -10,3 +13,8 @@ func _ready() -> void:
 			.translated(Vector3.UP * 0.25 + 9.8 * Vector3.RIGHT.rotated(Vector3.UP, y_rotation))
 		_spike_multimesh.set_instance_transform(i, spike_transform)
 	$SpikeMultiMeshInstance.show()
+
+	for i in pegs:
+		var peg := preload("res://table/peg.tscn").instantiate()
+		peg.translate(Vector3(peg_radius,0,0).rotated(Vector3.UP, TAU/6 * i))
+		add_child(peg)
